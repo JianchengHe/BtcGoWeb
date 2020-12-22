aaa943f58252e1fc4524fddf2c3f57108c2d3765
zhouzhijie3607154 <1843607154@qq.com>
2020-12-22 09:47:40 +0800

    test rpc connect

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ import (
 
 func main() {
 	fmt.Println("hello  world !")
-	command, err := btc.GetMsgByCommand("getblock", 0)
-	if err ！= nil {
-		
+	result, err := btc.GetMsgByCommand("getblockhash", 0)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+	fmt.Println(result.Result)
+
 	beego.Run()
+	/**
+	hello  world !
+	{"jsonrpc":"2.0","id":"1608601306","method":"getblockhash","params":[0]}
+	000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
+	 */
 
 }
 
